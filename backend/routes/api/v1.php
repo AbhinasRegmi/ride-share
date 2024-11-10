adcc8d87fc4b05d8d1a4092e4db3e8e72f94d253
abhinas regmi <aviregmi2018@gmail.com>
2024-11-10 22:16:49 +0545

    fix: api-version-fix: Manage api version prefix from bootstrap app

---
+++ backend/routes/api/v1.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\Api\V1\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware(["auth:sanctum"])
+    ->as("auth.")
+    ->group(
+        function () {
+
+            Route::get("/user", [AuthController::class, "user"])->name("user");
+        }
+    );
