c0cf95cf36b1ae778142a0d3694038803f57f9dc
abhinas regmi <aviregmi2018@gmail.com>
2024-11-10 23:25:14 +0545

    fix: migration-fix: Change constrained to actual tables

--- backend/database/migrations/2024_10_26_112049_create_drivers_table.php
+++ backend/database/migrations/2024_10_26_112049_create_drivers_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     {
         Schema::create('drivers', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained(User::class)->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
             $table->string('make');
             $table->string('model');
             $table->string('color');
